d747cae1aab5867fe37f67677820f91daf6ec7c9
badoowski <85910054+badoowski@users.noreply.github.com>
2023-01-18 21:17:30 +0100

    Update Exercise Level 3.js

--- 30 Days of JavaScript/Day 4/Exercise Level 3.js
+++ 30 Days of JavaScript/Day 4/Exercise Level 3.js
@@ -20,7 +20,7 @@
 
 // Answer
 let month = prompt('Enter a month').toLowerCase();
-let mmonth = month.charAt(0).toUpperCase() + month.slice(1);
+const mmonth = month.charAt(0).toUpperCase() + month.slice(1);
 if(month==='September'.toLowerCase() || month==='April'.toLowerCase() || month==='June'.toLowerCase() || month==='November'.toLowerCase()){
     console.log(mmonth + ' has 30 days.');
 }else if(month==='January'.toLowerCase() || month==='March'.toLowerCase() || month==='May'.toLowerCase() || month==='July'.toLowerCase()|| month==='August'.toLowerCase()|| month==='October'.toLowerCase()|| month==='December'.toLowerCase()){
@@ -37,7 +37,7 @@ if(month==='September'.toLowerCase() || month==='April'.toLowerCase() || month==
 
 // Answer
 let months = prompt('Enter a month').toLowerCase();
-let mmonths = months.charAt(0).toUpperCase() + months.slice(1);
+const mmonths = months.charAt(0).toUpperCase() + months.slice(1);
 if(months==='September'.toLowerCase() || months==='April'.toLowerCase() || months==='June'.toLowerCase() || months==='November'.toLowerCase()){
     console.log(mmonths + ' has 30 days.');
 }else if(months==='January'.toLowerCase() || months==='March'.toLowerCase() || months==='May'.toLowerCase() || months==='July'.toLowerCase()|| months==='August'.toLowerCase()|| months==='October'.toLowerCase()|| months==='December'.toLowerCase()){
